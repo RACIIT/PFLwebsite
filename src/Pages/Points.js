1346cf87f320b207481bc26301bbc89902a2b83c
TaalibAsaf <137051454+TaalibAsaf@users.noreply.github.com>
2024-04-27 17:18:59 +0530

    changed the fetch api to using google sheets

--- src/Pages/Points.js
+++ src/Pages/Points.js
@@ -12,114 +12,149 @@ import logo7 from "../Assets/tusker.PNG";
 import logo8 from "../Assets/valiente.PNG";
 
 export default function Points() {
+  const [teamRow, setTeamRow] = useState([]);
   const [teamData, setTeamData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [isGreenColor, setIsGreenColor] = useState(false); // Controlling the color
 
-  useEffect(() => {
-    const storedTeamData = localStorage.getItem("teamData");
-    if (storedTeamData) {
-      const parsedTeamData = JSON.parse(storedTeamData);
-      const sortedTeamData = parsedTeamData.sort((a, b) => {
-        if (b.points !== a.points) {
-          return b.points - a.points;
-        } else {
-          return b.gf - b.ga - (a.gf - a.ga);
-        }
-      });
 
-      setTimeout(() => {
-        setTeamData(sortedTeamData);
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/1E5ZSCPJY3Fc4_qqsGTNjHhRhDodRo0H-r7r_2i6tFVA/values/Points?alt=json&key=AIzaSyCPTSkOEQz2OmZNRlrhxXZPMqVgebH9X_I')
+      .then(response => response.json())
+      .then(data => {
+        const values = data.values;
+        // const transposedData = transpose(values);
+        console.log("data>>>>>>>>>>", values[2]);
+        setTeamRow(values);
         setLoading(false);
-      }, 3000);
-    }
+      })
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
+  // useEffect(() => {
+  //   const storedTeamData = localStorage.getItem("teamData");
+  //   if (storedTeamData) {
+  //     const parsedTeamData = JSON.parse(storedTeamData);
+  //     const sortedTeamData = parsedTeamData.sort((a, b) => {
+  //       if (b.points !== a.points) {
+  //         return b.points - a.points;
+  //       } else {
+  //         return b.gf - b.ga - (a.gf - a.ga);
+  //       }
+  //     });
+
+  //     setTeamData(sortedTeamData);
+  //       setLoading(false);
+  //   }
+  // }, []);
+
   useEffect(() => {
+    setLoading(false);
     setIsGreenColor(false); // Set isGreenColor to true when the useState is true
   }, [isGreenColor]);
 
-  return (
-    <div>
-      {loading ? (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            minHeight: "100vh",
-          }}
-        >
-          <Spinner animation="grow" variant="success" />
-        </div>
-      ) : (
-        <>
-          <img
-            src={pflText}
-            alt="Logo"
+  if (teamRow === null) {
+    return (<div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <Spinner animation="grow" variant="success" />
+    </div>);
+  } else {
+    return (
+      <div>
+        {loading ? (
+          <div
             style={{
-              display: "block",
-              margin: "auto",
-              width: "300px",
-              marginTop: "90px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "100vh",
             }}
-          />
-          <div style={{ marginTop: "50px" }}>
-            <div style={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}>
-              <div className="main-table">
-                <p className="card-header pos">Pos</p>
-                <p className="card-header name">Club Name</p>
-                <p className="card-header">MP</p>
-                <p className="card-header">W</p>
-                <p className="card-header">D</p>
-                <p className="card-header">L</p>
-                <p className="card-header">GF</p>
-                <p className="card-header">GA</p>
-                <p className="card-header">+/-</p>
-                <p className="card-header">Points</p>
-              </div>
-            </div>
-            {teamData.map((team, index) => (
-              <div
-                style={{ display: "flex", justifyContent: "center" }}
-                key={index}
-              >
-                <div className={`table ${isGreenColor && index < 4 ? "green-row" : ""}`}>
-                  <p className="card-header pos">{index + 1}</p>
-                  <p className="card-header name">
-                    <img
-                      src={getLogoByTeam(team.clubName)}
-                      style={{
-                        width: "44px",
-                        marginRight: "10px",
-                        backgroundColor: team.color,
-                        borderRadius: "50px",
-                        padding: "5px",
-                      }}
-                      alt="Logo"
-                    />
-                    {team.clubName}
-                  </p>
-                  <p className="card-header">{team.mp}</p>
-                  <p className="card-header">{team.win}</p>
-                  <p className="card-header">{team.draw}</p>
-                  <p className="card-header">{team.lose}</p>
-                  <p className="card-header">{team.gf}</p>
-                  <p className="card-header">{team.ga}</p>
-                  <p className="card-header">{team.gf - team.ga}</p>
-                  <p className="card-header">{team.points}</p>
+          >
+            <Spinner animation="grow" variant="success" />
+          </div>
+        ) : (
+          <>
+            <img
+              src={pflText}
+              alt="Logo"
+              style={{
+                display: "block",
+                margin: "auto",
+                width: "300px",
+                marginTop: "90px",
+              }}
+            />
+            <div style={{ marginTop: "50px" }}>
+              <div style={{ display: "flex", justifyContent: "center", marginBottom: "15px" }}>
+                <div className="main-table">
+                  {teamRow.length > 0 && ( // Check if teamRow has been fetched and is not empty
+                    <>
+                      <p className="card-header pos">{teamRow[0]?.[0]}</p>
+                      <p className="card-header name">{teamRow[0]?.[1]}</p>
+                      <p className="card-header">{teamRow[0]?.[2]}</p>
+                      <p className="card-header">{teamRow[0]?.[3]}</p>
+                      <p className="card-header">{teamRow[0]?.[4]}</p>
+                      <p className="card-header">{teamRow[0]?.[5]}</p>
+                      <p className="card-header">{teamRow[0]?.[6]}</p>
+                      <p className="card-header">{teamRow[0]?.[7]}</p>
+                      <p className="card-header">{teamRow[0]?.[8]}</p>
+                      <p className="card-header">{teamRow[0]?.[9]}</p>
+                    </>
+                  )}
                 </div>
               </div>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
-}
+              {teamRow.slice(1).map((team, index) => ( // Start from index 1 to skip the header row
+                <div key={index} style={{ display: "flex", justifyContent: "center" }}>
+                  <div className={`table ${isGreenColor && index < 4 ? "green-row" : ""}`}>
+                    <p className="card-header pos">{index + 1}</p>
+                    <p className="card-header name">
+                      <img
+                        src={getLogoByTeam(team[1])} // Assuming team[1] contains the clubName
+                        style={{
+                          width: "44px",
+                          marginRight: "10px",
+                          backgroundColor: team.color, // Assuming there's a color property in team
+                          borderRadius: "50px",
+                          padding: "5px",
+                        }}
+                        alt="Logo"
+                      />
+                      {team[1]} {/* Assuming team[1] contains the clubName */}
+                    </p>
+                    <p className="card-header">{team[2]}</p>
+                    <p className="card-header">{team[3]}</p>
+                    <p className="card-header">{team[4]}</p>
+                    <p className="card-header">{team[5]}</p>
+                    <p className="card-header">{team[6]}</p>
+                    <p className="card-header">{team[7]}</p>
+                    <p className="card-header">{team[8] - team[7]}</p>
+                    <p className="card-header">{team[9]}</p>
+                  </div>
+                </div>
+              ))}
 
+            </div>
+          </>
+        )}
+      </div>
+    );
+  }
+
+
+}
 function getLogoByTeam(clubName) {
-  switch (clubName) {
+  if (!clubName || clubName.trim() === "") {
+    return null; // Return null if clubName is empty or whitespace
+  }
+
+  switch (clubName.toUpperCase()) {
     case "AKELA":
       return logo1;
     case "CEROS":
@@ -140,3 +175,4 @@ function getLogoByTeam(clubName) {
       return null;
   }
 }
+
